a1470de72c066aa8e882497abfeadc2d4780c0c3
Marko Šacka <67114935+MarkoZemun@users.noreply.github.com>
2022-01-21 19:07:42 +0100

    Add files via upload

---
+++ FailedLoginTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import dataCreation.DataCreation;
+import methods.LoginMethods;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.VerificationPage;
+import utilities.PropertyManager;
+
+public class FailedLoginTest extends BaseTest{
+
+
+    @Test(dataProvider = "Data")
+    public void failedLogin(String email, String password){
+        LoginPage loginPage = new LoginPage(driver);
+        LoginMethods loginMethods = new LoginMethods(driver);
+        VerificationPage verificationPage = new VerificationPage(driver);
+
+        loginPage.basePage();
+        loginMethods.login(email, password);
+
+        try {
+            verificationPage.verifyFailedLogin("There is 1 error\nAuthentication failed.");
+            System.out.print("User is not logged in");
+        }catch (Exception e){
+            Assert.fail("User is logged in!");
+        }
+
+    }
+    @DataProvider(name="Data")
+    public Object [][] getDataFromDataProvider(){
+        return new Object[][]
+                {
+                        {DataCreation.fakeEmail(), DataCreation.fakePassword()},
+                        {DataCreation.fakeEmail(), DataCreation.fakePassword()},
+                        {DataCreation.fakeEmail(), DataCreation.fakePassword()}
+                };
+    }
+}
